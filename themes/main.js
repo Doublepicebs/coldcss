27c6aed40dd9c98118663b5590ff51341cffb289
doublepicebs <nananny2005@gmail.com>
2020-06-15 16:51:36 +0200

    c

--- themes/main.js
+++ themes/main.js
@@ -1,7 +1,14 @@
 Vue.component('featured-card', {
-  template: '<div class="rounded-lg bg-dark p-sm" style="width: max-content;"> <span style="display: flex; vertical-align: middle !important;"> <h4>{{name}}</h4><h5><ion-icon name="checkmark-circle" style="color: var(--primary) !important;"></ion-icon></h5> </span> <p>{{desc}}</p> <a href="{{path}}" class="button button-primary w-100">{{btn}}</a></div> <script>function pass() { var b = {{name}}.value, url = "themePage.html?name=" + encodeURIComponent(b); document.location.href = url;} </script>',
-  props: ['name', 'desc', 'path', 'btn']
+  template: '<div class="rounded-lg bg-dark p-sm" style="width: max-content;"> <span style="display: flex; vertical-align: middle !important;"> <h4>{{name}}</h4><h5><ion-icon name="checkmark-circle" style="color: var(--primary) !important;"></ion-icon></h5> </span> <p>{{desc}}</p> <a :href="path" :id="passed-info" class="button button-primary w-100 goToTheme">{{btn}}</a></div>',
+  props: ['name', 'desc', 'path', 'btn', 'passed-info']
 });
+
+for ( let i; i < $(".goToTheme").length; i++) {
+  $(".goToTheme")[i].click(function() {
+    localStorage.setItem("themeKey", $(".goToTheme")[i].attr("id"));
+    console.log(localStorage.getItem("themeKey"));
+  });
+}
 
 var app = new Vue({
  el: "#featuredComponents" 
